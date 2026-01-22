86723da256a856e9e397aea276113b47d45e5aa0
tanvirsingh551 <tanvir.singh05200704@gmail.com>
2026-01-23 00:13:16 +0530

    more pattern questions

---
+++ P8.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n = 3;
+    int num = 1;
+    for (int i = 0; i <= n - 1; i++)
+    {
+
+        for (int j = 1; j <= n; j++)
+        {
+
+            cout << num;
+            num++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ P9.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    char ch = 'A';
+    int n=4;
+    for(int i=0;i<=n;i++){
+         
+        for (int j=0;j<=n-1;j++){
+            cout<<ch;
+            ch=ch+1;
+        }
+    cout<<endl;}
+    return 0;
+}
