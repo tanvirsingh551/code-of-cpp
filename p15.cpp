ab19694a5d960665559d720dc36cdd300b8a1242
tanvirsingh551 <tanvir.singh05200704@gmail.com>
2026-01-23 00:38:02 +0530

    more pattern questionss

--- p15.cpp
+++ p15.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main()
 {
-    int n = 3;
+    int n = 6;
     int num = 1;
-    for (int i = 0; i <= n - 1; i++)
+    for (int i = 1; i <= n ; i++)
     {
 
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j <i; j++)
         {
 
-            cout << num;
+            cout << num<<" ";
             num++;
         }
         cout << endl;
